9116d53af8ea4883d30bf54bea3446867342d17a
PanChi <140990709+justpanchi@users.noreply.github.com>
2025-12-17 17:36:18 +0100

    Create setup.py

---
+++ khiscrape_wheel/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Setup script for KhiScrape."""
+
+from setuptools import setup
+
+setup(
+    name="khiscrape",
+    version="0.2025.12.17.0",
+    description="Asynchronous Khinsider Music Downloader",
+    author="PanChi (justpanchi)",
+    author_email="",
+    url="https://github.com/justpanchi/KhiScrape",
+    py_modules=["khiscrape"],
+    entry_points={
+        "console_scripts": [
+            "khiscrape = khiscrape:main_sync",
+        ],
+    },
+    install_requires=[
+        "aiohttp",
+        "aiofiles",
+        "beautifulsoup4",
+        "colorama",
+        "yarl",
+    ],
+    extras_require={
+        "lxml": ["lxml"],
+    },
+    python_requires=">=3.10",
+    license="MIT",
+    platforms=["any"],
+)
